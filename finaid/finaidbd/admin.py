7c09681aeb3fa8e94938e0b444624d26b7ac20a0
Md. Nazmul Islam Rumman <142523457+pomegstar@users.noreply.github.com>
2024-10-31 04:43:35 -0700

    Delete finaid directory

--- finaid/finaidbd/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import User, Deal_officer, Order, Installment
-
-# Register your models here.
-admin.site.register(User)
-admin.site.register(Deal_officer)
-admin.site.register(Order)
-admin.site.register(Installment)
